cec48af9edca11e485c1939169a99e8ce7f66c18
Mashaev Iulian <polizovateli@mail.ru>
2017-10-12 09:56:35 +0300

    :wink: created first java class

--- lab#2/README.md
+++ lab#2/README.md
@@ -6,7 +6,7 @@
  - [x] Folosit supraincarcarea operatorilor
 
 #### Java:
- - [ ] Creat clasa cu membru privat vector unidimensional de numere intregi, dimensiune variabila
- - [ ] Redefinit metoda **toString()**, care sa afiseze numerele continute
+ - [x] Creat clasa cu membru privat vector unidimensional de numere intregi, dimensiune variabila
+ - [x] Redefinit metoda **toString()**, care sa afiseze numerele continute
  - [ ] In functia **main()** construiti un obiect din clasa creata cu numerele (2,9,4,5,7,8)
  - [ ] Utilizarea metodei **toString()**
---
+++ lab#2/java/Lista.java
@@ -0,0 +1,22 @@
+import java.lang.Integer;
+import java.lang.String;
+import java.util.ArrayList;
+
+public class Lista {
+    private ArrayList<Integer> vector = new ArrayList<Integer>();
+
+    public Lista() {}
+
+    public Lista(ArrayList<Integer> vector) {
+        this.vector = vector;
+    }
+
+    public String toString() {
+        String listString = "{";
+        for (int i : vector) {
+            listString += String.valueOf(i) + ", ";
+        }
+        listString = listString.substring(0, listString.length()-2) + "}";
+        return listString;
+    }
+}
