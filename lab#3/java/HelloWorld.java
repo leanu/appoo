d727321a62c2985e26dd3b666714765fff8ae64c
Mashaev Iulian <polizovateli@mail.ru>
2017-10-14 10:21:29 +0300

    :blush: applet as independent application

--- lab#3/java/HelloWorld.java
+++ lab#3/java/HelloWorld.java
@@ -1,10 +1,32 @@
 import java.applet.Applet;
 import java.awt.*;
 import java.applet.*;
+import java.awt.BorderLayout;
+import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.lang.Override;
 
 public class HelloWorld extends Applet {
     public void paint(Graphics g) {
         g.drawString("Hello World!", 10, 10);
     }
+
+    public static void main(String[] args) {
+        HelloWorld applet = new HelloWorld();
+        Frame frame = new Frame("Frame Applet");
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        frame.add(applet, BorderLayout.CENTER);
+        frame.setSize(300,200);
+        applet.init();
+        applet.start();
+        frame.setVisible(true);
+    }
+
 }
