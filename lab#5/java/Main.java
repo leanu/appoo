01c7f0adce912c9aa99ec66014e1b78ebc506160
Mashaev Iulian <polizovateli@mail.ru>
2017-10-14 14:04:22 +0300

    :stuck_out_tongue_winking_eye: usage of bytearrayoutputstream

--- lab#5/java/Main.java
+++ lab#5/java/Main.java
@@ -1,4 +1,7 @@
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.lang.System;
 
 class Main {
     public static void main(String[] args) {
@@ -9,5 +12,14 @@ class Main {
         while((byteRead = bufferStream.read()) != -1)
             System.out.print(" " + byteRead);
         System.out.println();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(256);
+        try{
+            for(int i=0;i<256;i++) outputStream.write(i);
+            outputStream.writeTo(System.out);
+            byte[] tablou = outputStream.toByteArray();
+        } catch (IOException e) {
+            System.out.println("Erroare "+ e.getMessage());
+        }
     }
 }
